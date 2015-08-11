92208357be8a1dcddecab51fd00588c772f18ce7
birajpjpt <birajprajapati@hotmail.com>
2015-08-11 20:42:07 +0100

    new c# file added

---
+++ AESDecryptor.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace ConsoleApplicationReadFromDatabaseAES
+{
+    class AESDecryptor
+    {
+        public static string Decryptor(string TextToDecrypt, string strKey)
+        {
+            byte[] EncryptedBytes = Convert.FromBase64String(TextToDecrypt);
+            string strIV = "0123456789ABCDEF";
+            //Setup the AES provider for decrypting.            
+            AesCryptoServiceProvider aesProvider = new AesCryptoServiceProvider();
+            //aesProvider.Key = System.Text.Encoding.ASCII.GetBytes(strKey);
+            //aesProvider.IV = System.Text.Encoding.ASCII.GetBytes(strIV);
+            aesProvider.BlockSize = 128;
+            aesProvider.KeySize = 256;
+
+            aesProvider.Key = stringToByte(strKey, 32);
+            aesProvider.IV = string2byte(strIV);
+            aesProvider.Padding = PaddingMode.PKCS7;
+            aesProvider.Mode = CipherMode.CBC;
+
+
+            ICryptoTransform cryptoTransform = aesProvider.CreateDecryptor(aesProvider.Key, aesProvider.IV);
+            byte[] DecryptedBytes = cryptoTransform.TransformFinalBlock(EncryptedBytes, 0, EncryptedBytes.Length);
+            return System.Text.Encoding.ASCII.GetString(DecryptedBytes);
+        }
+
+        public static byte[] string2byte(string newString)
+        {
+            char[] CharArray = newString.ToCharArray();
+            byte[] ByteArray = new byte[CharArray.Length];
+
+            for (int i = 0; i < CharArray.Length; i++)
+            {
+                ByteArray[i] = Convert.ToByte(CharArray[i]);
+            }
+            return ByteArray;
+        }
+
+        public static byte[] stringToByte(string newString, int charLength)
+        {
+            char[] CharArray = newString.ToCharArray();
+            byte[] ByteArray = new byte[charLength];
+            for (int i = 0; i < CharArray.Length; i++)
+            {
+                ByteArray[i] = Convert.ToByte(CharArray[i]);
+            }
+            return ByteArray;
+        }
+    }
+}
